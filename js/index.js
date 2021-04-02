481691a033921c8d8f985242df77be9d289bed98
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-01 20:38:22 -0600

    Show Answers FAQ

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 'use strict'
 document.querySelector('.content-body-questions').addEventListener('click', ()=>{
     var parrafo =document.querySelector('p');
+    var question = document.querySelector('h2');
     
     
     if(parrafo.style.display == "block"){
         parrafo.style.display = "none"
+        question.style.fontWeight = "normal"
     } else{
         parrafo.style.display = "block";
+        question.style.fontWeight = "bold"
     }
 });
