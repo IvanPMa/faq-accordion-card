e0eea3f2f5b90efc8b9b121a3ff1fbab33f19180
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-01 21:17:31 -0600

    showAnswer Function

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 'use strict'
-
-document.querySelector('.one').addEventListener('click', ()=>{
-    var answer =document.getElementById('answer-1')
-    var question = document.getElementById('question-1');
-    
-    
+// function that displays the answers and styles
+function showAnswer(answer,question){
     if(answer.style.display == "block"){
         answer.style.display = "none"
         question.style.fontWeight = "normal"
@@ -12,11 +8,16 @@ document.querySelector('.one').addEventListener('click', ()=>{
         answer.style.display = "block";
         question.style.fontWeight = "bold"
     }
+}
+
+// First question
+document.querySelector('.one').addEventListener('click', ()=>{
+    var answer =document.getElementById('answer-1')
+    var question = document.getElementById('question-1');
+    showAnswer(answer,question);
+    
 });
 
 
 
-function showAnswer(){
-    
-}
 
